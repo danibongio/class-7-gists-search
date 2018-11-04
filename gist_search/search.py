d8d2fdd8d8853ce8d696389c280dd8192c8eef20
Daniele Bongiovanni <danibongio@gmail.com>
2018-11-04 00:01:12 +0000

    all tests passed

--- gist_search/search.py
+++ gist_search/search.py
@@ -5,3 +5,25 @@ def search_gists(username, description=None, file_name=None):
     if not description and not file_name:
         print("At least one search parameter must be specified")
         return
+    gists = get_gists(username)
+    
+    result=[]
+    
+    for gist in gists:
+        #first condition description
+        if description and description.lower() not in gist['description'].lower():
+            continue
+        
+        #second condition file_name
+        if file_name:
+            matched=False
+            for key, values in gist['files'].items():
+                if file_name.lower() in key.lower():
+                    matched=True
+            if not matched:
+                continue
+            
+        result.append(gist)
+        
+    return result
+    
